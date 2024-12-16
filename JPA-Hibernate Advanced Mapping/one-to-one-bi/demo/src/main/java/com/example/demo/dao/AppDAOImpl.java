65811be18204954ee1b2c2e28a3ef731952c148b
HAMZA-NASIR-PU <hamzanasir1111.hn@gmail.com>
2024-12-16 16:20:44 +0500

    Hibernate Advanced Mapping - One -to-One Bi-directional

---
+++ JPA-Hibernate Advanced Mapping/one-to-one-bi/demo/src/main/java/com/example/demo/dao/AppDAOImpl.java
@@ -0,0 +1,50 @@
+package com.example.demo.dao;
+
+import com.example.demo.entity.Instructor;
+import com.example.demo.entity.InstructorDetail;
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class AppDAOImpl implements AppDAO{
+
+    // define field for entity manager
+
+    // inject entity manager using constructor injection
+
+    private EntityManager entityManager;
+
+    @Autowired // just for readability purpose.
+    public AppDAOImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    public Instructor findInstructorById(int id) {
+        // This will also retrieve the instructor details object because of default behaviour of @OneToOne fetch type is eager.
+        return entityManager.find(Instructor.class, id);
+    }
+
+    @Override
+    @Transactional
+    public void deleteInstructorById(int id) {
+        //This will also delete the instructorDetail object because of CascadeType.ALL
+        Instructor instructor = entityManager.find(Instructor.class, id);
+        entityManager.remove(instructor);
+    }
+
+    @Override
+    public InstructorDetail findInstructorDetailById(int id) {
+        return entityManager.find(InstructorDetail.class, id);
+    }
+
+
+    @Override
+    @Transactional
+    public void save(Instructor instructor) {
+        // This will also save the InstructorDetail object because of CascadeType.ALL
+        entityManager.persist(instructor);
+    }
+}

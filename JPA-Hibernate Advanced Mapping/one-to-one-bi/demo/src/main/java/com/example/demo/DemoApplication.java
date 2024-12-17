d3967864a2a2ff67fe53ac20c357250349137156
HAMZA-NASIR-PU <hamzanasir1111.hn@gmail.com>
2024-12-17 16:08:20 +0500

    Clean unidriectional project files

--- JPA-Hibernate Advanced Mapping/one-to-one-bi/demo/src/main/java/com/example/demo/DemoApplication.java
+++ JPA-Hibernate Advanced Mapping/one-to-one-bi/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,8 +1,5 @@
 package com.example.demo;
 
-import com.example.demo.dao.AppDAO;
-import com.example.demo.entity.Instructor;
-import com.example.demo.entity.InstructorDetail;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -16,65 +13,10 @@ public class DemoApplication {
 	}
 
 	@Bean
-	public CommandLineRunner commandLineRunner(AppDAO appDAO) {
-
+	public CommandLineRunner commandLineRunner() {
 		return runner -> {
-//			findInstructor(appDAO);
-//			deleteInstructor(appDAO);
-//			createInstructor(appDAO);
-			findInstructorDetail(appDAO);
 		};
 
-	}
-
-	private void findInstructorDetail(AppDAO appDAO) {
-		int id = 2;
-		InstructorDetail instructorDetail = appDAO.findInstructorDetailById(id);
-		System.out.println(instructorDetail);
-
-		System.out.println("Associated Instructor object = " + instructorDetail.getInstructor());
-	}
-
-	private void deleteInstructor(AppDAO appDAO) {
-		int id = 1;
-		System.out.println("Finding Instructor id: " + id);
-		Instructor instructor = appDAO.findInstructorById(id);
-		System.out.println(instructor);
-		System.out.println("The associated instructorDetail only: " + instructor.getInstructorDetail());
-		appDAO.deleteInstructorById(id);
-
-		System.out.println("Deleted instructor with id: " + id);
-	}
-
-	private void findInstructor(AppDAO appDAO) {
-		int id = 2;
-		System.out.println("Finding Instructor id: " + id);
-		Instructor instructor = appDAO.findInstructorById(id);
-		System.out.println(instructor);
-		System.out.println("The associated instructorDetail only: " + instructor.getInstructorDetail());
-	}
-
-	private void createInstructor(AppDAO appDAO) {
-//		Instructor instructor = new Instructor("Chad", "Darby", "chad@email.com");
-//		InstructorDetail instructorDetail = new InstructorDetail(
-//				"http://www.youtube.com/abc",
-//				"gaming"
-//		);
-//		instructor.setInstructorDetail(instructorDetail);
-
-		Instructor instructor = new Instructor("Michjael", "Jordan", "michael@email.com");
-		InstructorDetail instructorDetail = new InstructorDetail(
-				"http://www.youtube.com/michael",
-				"guitar"
-		);
-		instructor.setInstructorDetail(instructorDetail);
-
-		// NOTE: This will also save the details object because of CascadeType.ALL.
-
-		// Inserts the associated entity first: InstructorDetail. Then inserts: Instructor
-		// Due to relationship of foreign key Instructor needs to know the id of the InstructorDetail
-		appDAO.save(instructor);
-		System.out.println("Saving instructor " + instructor);
 	}
 
 }
